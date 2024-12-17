bb91da3c0d36b3b36e0e779e9ef9e23066a02a9f
jeayeob <radic2510@gmail.com>
2024-12-17 23:28:49 +0900

    init study

---
+++ src/main/kotlin/com/yobi/study/api_dev/ApiDevApplication.kt
@@ -0,0 +1,11 @@
+package com.yobi.study.api_dev
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiDevApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiDevApplication>(*args)
+}
---
+++ src/test/kotlin/com/yobi/study/api_dev/ApiDevApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yobi.study.api_dev
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiDevApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
